b1e9c0bb239acf1c112dc82b6877fc061cb6e2f0
sku6 <sku6@illinois.edu>
2021-04-20 18:37:21 -0500

    Implement GetPosition() and GetColor() in snake.cc

--- include/snake.h
+++ include/snake.h
@@ -1,26 +1,29 @@
 #pragma warning(disable : 4819)
 #pragma once
 
+#include <cinder/Color.h>
+
 namespace snake {
 class Snake {
  public:
   /**
-   * Constructor
+   * Constructor that takes in the position as a parameter
    */
-  Snake();
+  Snake(const glm::vec2& position);
 
   /**
    * Adds a new part to the end of the snake once the snake eats food
    */
   void addPart();
 
-  /**
-   * @return Size(length) of the snake
-   */
-  size_t GetSize();
+  size_t GetSize() const;
+  ci::Color GetColor() const;
+  glm::vec2 GetPosition() const;
 
  private:
   size_t size_;
+  const char* kSnakeColor = "green";
+  glm::vec2 position_;
 };
 }  // namespace snake
 
--- src/snake.cc
+++ src/snake.cc
@@ -1,14 +1,23 @@
 #include "snake.h"
+using glm::vec2;
+
 namespace snake {
-snake::Snake::Snake() {
+snake::Snake::Snake(const vec2& position) {
   size_ = 1;
+  position_ = position;
 }
 
 void Snake::addPart() {
   ++size_;
 }
 
-size_t Snake::GetSize() {
+size_t Snake::GetSize() const {
   return size_;
+}
+ci::Color Snake::GetColor() const {
+  return kSnakeColor;
+}
+glm::vec2 Snake::GetPosition() const {
+  return position_;
 }
 }  // namespace snake
