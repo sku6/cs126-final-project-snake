dd152aeebb0298b4dc9cdb9d7df4852c057c144e
sku6 <sku6@illinois.edu>
2021-04-20 16:42:06 -0500

    Adjust CMakeLists.txt to finish building

--- include/boarder.h
+++ include/boarder.h
@@ -1,8 +1,31 @@
-//
-// Created by hp on 4/19/2021.
-//
+#pragma warning(disable : 4819)
+#pragma once
 
-#ifndef FINAL_PROJECT_SKU6_BOARDER_H
-#define FINAL_PROJECT_SKU6_BOARDER_H
+#include "cinder/gl/gl.h"
 
-#endif  // FINAL_PROJECT_SKU6_BOARDER_H
+namespace snake {
+ class Boarder {
+  public:
+   /**
+    * Using the top left corner and the bottom right corner to create a box for the snake game
+    */
+   Boarder(const glm::vec2& top_left_corner, const glm::vec2& bottom_right_corner);
+
+   /**
+   * Displays the container walls and the current positions of the particles.
+   */
+   void Display() const;
+
+   /**
+   * Updates the positions and velocities of all particles (based on the rules
+   * described in the assignment documentation).
+   */
+   void AdvanceOneFrame();
+
+  private:
+   // Create container
+   const char* kContainerColor = "white";
+   glm::vec2 container_bottom_right_corner_;
+   glm::vec2 container_top_left_corner_;
+ };
+}
--- src/boarder.cc
+++ src/boarder.cc
@@ -1,5 +1,21 @@
-//
-// Created by hp on 4/19/2021.
-//
-
 #include "boarder.h"
+using glm::vec2;
+
+namespace snake {
+snake::Boarder::Boarder(const glm::vec2& top_left_corner,
+                        const glm::vec2& bottom_right_corner) {
+  container_top_left_corner_ = top_left_corner;
+  container_bottom_right_corner_ = bottom_right_corner;
+}
+
+void Boarder::Display() const {
+  // Draw the container
+  ci::gl::color(ci::Color(kContainerColor));
+  ci::gl::drawStrokedRect(
+      ci::Rectf(container_top_left_corner_, container_bottom_right_corner_));
+}
+
+void Boarder::AdvanceOneFrame() {
+
+}
+}  // namespace snake
