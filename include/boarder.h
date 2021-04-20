2618b9e8a9d2b173920d8faf7076f314cfffc7e8
sku6 <sku6@illinois.edu>
2021-04-19 23:10:41 -0500

    Create boarder class

---
+++ include/boarder.h
@@ -0,0 +1,8 @@
+//
+// Created by hp on 4/19/2021.
+//
+
+#ifndef FINAL_PROJECT_SKU6_BOARDER_H
+#define FINAL_PROJECT_SKU6_BOARDER_H
+
+#endif  // FINAL_PROJECT_SKU6_BOARDER_H
---
+++ src/boarder.cc
@@ -0,0 +1,5 @@
+//
+// Created by hp on 4/19/2021.
+//
+
+#include "boarder.h"
