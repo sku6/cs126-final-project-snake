da0367dd2e2c63335b47cd7f38ac96d335e90381
sku6 <sku6@illinois.edu>
2021-05-04 00:49:24 -0500

    Implement movement functions for the extensions

--- include/extension.h
+++ include/extension.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <glm/vec2.hpp>
+#include "snake.h"
 
 namespace snake {
 class Extension {
@@ -14,7 +15,30 @@ class Extension {
   glm::vec2 GetPosition() const;
   void SetPosition(const glm::vec2 position);
 
+  /**
+ * Make the snake move upwards
+ */
+  void MoveUp();
+
+  /**
+   * Make the snake move downwards
+   */
+  void MoveDown();
+
+  /**
+   * Make the snake move to the left
+   */
+  void MoveLeft();
+
+  /**
+   * Make the snake move to the right
+   */
+  void MoveRight();
+
+  size_t GetKMoveIncrement() const;
+
  private:
   glm::vec2 position_;
+  size_t kMoveIncrement = 1;
 };
 }  // namespace snake
--- src/extension.cc
+++ src/extension.cc
@@ -12,5 +12,22 @@ vec2 Extension::GetPosition() const{
 void Extension::SetPosition(const glm::vec2 position) {
   position_ = position;
 }
+void Extension::MoveUp(){
+  position_ = vec2(GetPosition().x, GetPosition().y-kMoveIncrement);
+}
 
+void Extension::MoveDown(){
+  position_ = vec2(GetPosition().x, GetPosition().y+kMoveIncrement);
+}
+
+void Extension::MoveLeft(){
+  position_ = vec2(GetPosition().x-kMoveIncrement, GetPosition().y);
+}
+
+void Extension::MoveRight() {
+  position_ = vec2(GetPosition().x+kMoveIncrement, GetPosition().y);
+}
+size_t Extension::GetKMoveIncrement() const {
+  return kMoveIncrement;
+}
 }
