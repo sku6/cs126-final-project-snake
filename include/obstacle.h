2025ae9b4a280e34fe791c61c9c038557d4142b3
sku6 <sku6@illinois.edu>
2021-05-03 22:12:54 -0500

    Implement getter and setter functions in class Obstacle

--- include/obstacle.h
+++ include/obstacle.h
@@ -1,8 +1,34 @@
-//
-// Created by hp on 5/3/2021.
-//
+#pragma warning(disable : 4819)
+#pragma once
 
-#ifndef FINAL_PROJECT_SKU6_OBSTACLE_H
-#define FINAL_PROJECT_SKU6_OBSTACLE_H
+#include <cinder/Color.h>
+#include "utilities.h"
 
-#endif  // FINAL_PROJECT_SKU6_OBSTACLE_H
+namespace snake {
+class Obstacle {
+ public:
+  /**
+   * Default constructor
+   */
+  Obstacle();
+
+  /**
+   * Constructor that takes in a vec2 position as parameter
+   */
+  Obstacle(glm::vec2 position);
+
+  /**
+ * Create a new position for the obstacle
+ * Decrement score if snake hits an obstacle
+ * @return New position for the obstacle
+ */
+  glm::vec2 SetNewPosition();
+
+  ci::Color GetColor() const;
+  glm::vec2 GetPosition() const;
+
+ private:
+  glm::vec2 position_;
+  const char* kObstacleColor = "blue";
+};
+}
--- src/obstacle.cc
+++ src/obstacle.cc
@@ -1,5 +1,25 @@
-//
-// Created by hp on 5/3/2021.
-//
-
 #include "obstacle.h"
+using glm::vec2;
+namespace snake {
+
+snake::Obstacle::Obstacle() {
+  position_ = vec2(Utilities::GenerateRandomNumberBetween(101, 699),Utilities::GenerateRandomNumberBetween(101, 699));
+}
+
+Obstacle::Obstacle(glm::vec2 position) {
+  position_ = position;
+}
+
+glm::vec2 Obstacle::SetNewPosition() {
+  position_ = vec2(Utilities::GenerateRandomNumberBetween(101, 699),Utilities::GenerateRandomNumberBetween(101, 699));
+  return position_;
+}
+
+ci::Color Obstacle::GetColor() const {
+  return kObstacleColor;
+}
+
+glm::vec2 Obstacle::GetPosition() const {
+  return position_;
+}
+}
